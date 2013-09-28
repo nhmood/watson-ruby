c3d699375258fb634df3165c1b16206971f5fb55
nhmood <nhmood@goosecode.com>
2013-09-28 17:12:07 -0400

    Updated debug_print to support static methods
    Added logic to debug_print check to allow for static methods and
    class methods to both be supported
    Accessing class constant differs slightly in syntax between the two

--- lib/watson.rb
+++ lib/watson.rb
@@ -9,8 +9,8 @@ module Watson
 	# Separate ON and OFF so we can force state and still let 
 	# individual classes have some control over their prints
 	 
-	GLOBAL_DEBUG_ON = true
-	GLOBAL_DEBUG_OFF = true
+	GLOBAL_DEBUG_ON = false
+	GLOBAL_DEBUG_OFF = false 
 
 	# [review] - Not sure if module_function is proper way to scope
 	# I want to be able to call debug_print without having to use the scope
@@ -23,7 +23,13 @@ module Watson
 		# Print only if DEBUG flag of calling class is true OR 
 		# GLOBAL_DEBUG_ON of Watson module (defined above) is true
 		# AND GLOBAL_DEBUG_OFF of Watson module (Defined above) is false
-		print msg if ( (self.class::DEBUG == true || GLOBAL_DEBUG_ON == true) && (GLOBAL_DEBUG_OFF == false))
+
+		# Sometimes we call debug_print from a static method (class << self)
+		# and other times from a class method, and ::DEBUG is accessed differently 
+		# from a class vs object, so lets take care of that
+		_DEBUG = (self.is_a? Class) ? self::DEBUG : self.class::DEBUG
+
+		print "=> #{msg}" if ( (_DEBUG == true || GLOBAL_DEBUG_ON == true) && (GLOBAL_DEBUG_OFF == false))
 	end	
 
 end
