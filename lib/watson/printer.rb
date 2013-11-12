b9fa910defae8906dce5e759ece875d8e9a0a866
nhmood <nhmood@goosecode.com>
2013-11-12 18:46:29 -0500

    Code scrub for printer.rb
    Ruby code cleanup for printer.rb and necessary changes in other files

--- lib/watson/printer.rb
+++ lib/watson/printer.rb
@@ -8,7 +8,6 @@ module Watson
 		UNDERLINE 	= "\e[4m"
 		RESET 		= "\e[00m"
 
-		# Standard colors
 		GRAY	= "\e[38;5;0m"
 		RED		= "\e[38;5;1m"
 		GREEN	= "\e[38;5;2m"
@@ -19,28 +18,30 @@ module Watson
 		WHITE	= "\e[38;5;7m"
 
 
+	# Printer class that handles all formatting and printing of parsed dir/file structure
 	class Printer
 		# [review] - Not sure if the way static methods are defined is correct
 		#			 Ok to have same name as instance methods?
 		#			 Only difference is where the output gets printed to
 		# [review] - No real setup in initialize method, combine it and run method?
 
-		# Include for debug_print
+		# Include for debug_print (for class methods)
 		include Watson
 
-		# Class Constants
-		DEBUG = false 		# Debug printing for this class
+		# Debug printing for this class
+		DEBUG = false 		
 
 		class << self
+	
+		# Include for debug_print (for static methods)
 		include Watson
 
 		###########################################################
-		# cprint 
-		###########################################################
-		
+		# Custom color print for static call (only writes to STDOUT)   
 		def cprint (msg = "", color = "")
+
 			# Identify method entry
-			debug_print "#{self} : #{__method__}\n"
+			debug_print "#{ self } : #{ __method__ }\n"
 		
 			# This little check will allow us to take a Constant defined color
 			# As well as a [0-256] value if specified
@@ -49,21 +50,19 @@ module Watson
 				STDOUT.write(color)
 			elsif (color.between?(0, 256))
 				debug_print "No or Default color specified for cprint\n"
-				STDOUT.write("\e[38;5;#{color}m")
+				STDOUT.write("\e[38;5;#{ color }m")
 			end
 		
 			STDOUT.write(msg)
-		
 		end
 
 
 		###########################################################
-		# print_header 
-		###########################################################
-
+		# Standard header print for static call (uses static cprint) 
 		def print_header 
+		
 			# Identify method entry
-			debug_print "#{self} : #{__method__}\n"
+			debug_print "#{ self } : #{ __method__ }\n"
 
 			# Header
 			cprint BOLD + "------------------------------\n" + RESET
@@ -76,27 +75,24 @@ module Watson
 		end
 
 
-	
 		###########################################################
-		# print_status 
-		###########################################################
-	
+		# Status printer for static call (uses static cprint) 
+		# Print status block in standard format
 		def print_status(msg, color) 
 			cprint RESET + BOLD
 			cprint WHITE + "[ "
-			cprint "#{msg} ", color
+			cprint "#{ msg } ", color
 			cprint WHITE + "] " + RESET
 		end
 
 		end
 
 		###########################################################
-		# initialize 
-		###########################################################
-	
+		# Printer initialization method to setup necessary parameters, states, and vars
 		def initialize(config)
+
 			# Identify method entry
-			debug_print "#{self} : #{__method__}\n"
+			debug_print "#{ self } : #{ __method__ }\n"
 			
 			@config = config
 			return true
@@ -104,18 +100,17 @@ module Watson
 
 		
 		###########################################################
-		# run 
-		###########################################################
-
+		# Take parsed structure and print out in specified formatting 
 		def run(structure)
+
 			# Identify method entry
-			debug_print "#{self} : #{__method__}\n"
+			debug_print "#{ self } : #{ __method__ }\n"
 		
 			# Check Config to see if we have access to less for printing
 			# If so, open our temp file as the output to write to
 			# Else, just print out to STDOUT
-			if (@config.use_less)
-				debug_print "Unix less avaliable, setting output to #{@config.tmp_file}\n"
+			if @config.use_less
+				debug_print "Unix less avaliable, setting output to #{ @config.tmp_file }\n"
 				@output = File.open(@config.tmp_file, 'w')
 			elsif
 				debug_print "Unix less is unavaliable, setting output to STDOUT\n"
@@ -131,9 +126,10 @@ module Watson
 			print_structure(structure)
 
 			# If we are using less, close the output file, display with less, then delete
-			if (@config.use_less)
-				@output.close	
-				system("less #{@config.tmp_file}")
+			if @config.use_less
+				@output.close
+				# [review] - Way of calling a native Ruby less?
+				system("less #{ @config.tmp_file }")
 				debug_print "File displayed with less, now deleting...\n"
 				File.delete(@config.tmp_file)
 			end
@@ -141,35 +137,14 @@ module Watson
 			return true
 		end
 
-	
-		###########################################################
-		# print_header 
-		###########################################################
-
-		def print_header 
-			# Identify method entry
-			debug_print "#{self} : #{__method__}\n"
-
-			# Header
-			cprint BOLD + "------------------------------\n" + RESET
-			cprint BOLD + "watson" + RESET
-			cprint " - " + RESET
-			cprint BOLD + YELLOW + "inline issue manager\n\n" + RESET
-			cprint "Run in: #{Dir.pwd}\n"
-			cprint "Run @ #{Time.now.asctime}\n"
-			cprint BOLD + "------------------------------\n\n" + RESET
-
-			return true
-		end
-
 
 		###########################################################
-		# cprint 
-		###########################################################
-
+		# Custom color print for member call
+		# Allows not only for custom color printing but writing to file vs STDOUT
 		def cprint (msg = "", color = "")
+
 			# Identify method entry
-			debug_print "#{self} : #{__method__}\n"
+			debug_print "#{ self } : #{ __method__ }\n"
 
 			# This little check will allow us to take a Constant defined color
 			# As well as a [0-256] value if specified
@@ -178,116 +153,139 @@ module Watson
 				@output.write(color)
 			elsif (color.between?(0, 256))
 				debug_print "No or Default color specified for cprint\n"
-				@output.write("\e[38;5;#{color}m")
+				@output.write("\e[38;5;#{ color }m")
 			end
 
 			@output.write(msg)
-
 		end
 
 	
 		###########################################################
-		# print_status 
+		# Standard header print for class call (uses member cprint)
+		def print_header 
+			# Identify method entry
+
+			debug_print "#{ self } : #{ __method__ }\n"
+
+			# Header
+			cprint BOLD + "------------------------------\n" + RESET
+			cprint BOLD + "watson" + RESET
+			cprint " - " + RESET
+			cprint BOLD + YELLOW + "inline issue manager\n\n" + RESET
+			cprint "Run in: #{ Dir.pwd }\n"
+			cprint "Run @ #{ Time.now.asctime }\n"
+			cprint BOLD + "------------------------------\n\n" + RESET
+
+			return true
+		end
+
+
 		###########################################################
-	
+		# Status printer for member call (uses member cprint)
+		# Print status block in standard format
 		def print_status(msg, color) 
 			cprint RESET + BOLD
 			cprint WHITE + "[ "
-			cprint "#{msg} ", color
+			cprint "#{ msg } ", color
 			cprint WHITE + "] " + RESET
 		end
 
 
 		###########################################################
-		# print_structure 
-		###########################################################
-	
+		# Go through all files and directories and call necessary printing methods
+		# Print all individual entries, call print_structure on each subdir  
 		def print_structure(structure)
+
 			# Identify method entry
-			debug_print "#{self} : #{__method__}\n"
+			debug_print "#{ self } : #{ __method__ }\n"
 
 			# First go through all the files in the current structure
 			# The current "structure" should reflect a dir/subdir
 			structure[:files].each do | _file |
-				debug_print "Printing info for #{_file}\n"
+				debug_print "Printing info for #{ _file }\n"
 				print_entry(_file)
 			end
 
 			# Next go through all the subdirs and pass them to print_structure
 			structure[:subdirs].each do | _subdir |
-				debug_print "Entering #{_subdir} to print further\n"
+				debug_print "Entering #{ _subdir } to print further\n"
 				print_structure(_subdir)
 			end
-
-
 		end
 
 
 		###########################################################
-		# print_entry
-		###########################################################
-		
+		# Individual entry printer
+		# Uses issue hash to format printed output
 		def print_entry(entry)
+
 			# Identify method entry
-			debug_print "#{self} : #{__method__}\n"
+			debug_print "#{ self } : #{ __method__ }\n"
 
 			# If no issues for this file, print that and break
 			# The filename print is repetative, but reduces another check later
-			if (entry[:has_issues] == false)
-				debug_print "No issues for #{entry}\n"
+			if entry[:has_issues] == false
+				debug_print "No issues for #{ entry }\n"
 				print_status "o", GREEN
-				cprint BOLD + UNDERLINE + GREEN + "#{entry[:relative_path]}" + RESET + "\n"
+				cprint BOLD + UNDERLINE + GREEN + "#{ entry[:relative_path] }" + RESET + "\n"
 				return true
 			else
-				debug_print "No issues for #{entry}\n"
+				debug_print "Issues found for #{ entry }\n"
 				cprint "\n"
 				print_status "x", RED
 				cprint BOLD + UNDERLINE + RED + "#{entry[:relative_path]}" + RESET + "\n"
 			end	
+
 	
 			# [review] - Should the tag structure be self contained in the hash
 			#			 Or is it ok to reference @config to figure out the tags
 			@config.tag_list.each do | _tag |
-				debug_print "Checking for #{_tag}\n"
+				debug_print "Checking for #{ _tag }\n"
 
 				# [review] - Better way to ignore tags through structure (hash) data
 				# Maybe have individual has_issues for each one?
-				if (entry[_tag].size > 0 )
-					debug_print "#{_tag} has issues in it, print!\n"
-					print_status "#{_tag}", BLUE
+				if entry[_tag].size.zero?
+					debug_print "#{ _tag } has no issues, skipping\n"
 					cprint "\n"
-					entry[_tag].each do | _issue |
-						cprint WHITE + "  line #{_issue[:line_number]} - " + RESET
-						cprint BOLD + "#{_issue[:title]}" + RESET
+					next
+				end
+				
+				debug_print "#{ _tag } has issues in it, print!\n"
+				print_status "#{ _tag }", BLUE
+				cprint "\n"
+
+				# Go through each issue in tag
+				entry[_tag].each do | _issue |
+					cprint WHITE + "  line #{ _issue[:line_number] } - " + RESET
+					cprint BOLD + "#{ _issue[:title] }" + RESET
 
 
-						# Check to see if it has been resolved on GitHub/Bitbucket
-						debug_print "Checking if issue has been resolved\n"	
-						@config.github_issues[:closed].each do | _closed | 
-							if (_closed["body"].include?(_issue[:md5]))
-								debug_print "Found in #{_closed[:comment]}, not posting\n"
-								cprint BOLD + " [" + RESET
-								cprint GREEN + BOLD + "Resolved on GitHub" + RESET
-								cprint BOLD + "]" + RESET
-							end
-							debug_print "Did not find in #{_closed[:comment]}\n"
-						end	
-
-						debug_print "Checking if issue has been resolved\n"
-						@config.bitbucket_issues[:closed].each do  | _closed | 
-							if (_closed["content"].include?(_issue[:md5]))
-								debug_print "Found in #{_closed["content"]}, not posting\n"
-								cprint BOLD + " [" + RESET
-								cprint GREEN + BOLD + "Resolved on Bitbucket" + RESET
-								cprint BOLD + "]\n" + RESET
-							end
-							debug_print "Did not find in #{_closed["title"]}\n"
+					# Check to see if it has been resolved on GitHub/Bitbucket
+					debug_print "Checking if issue has been resolved\n"	
+					@config.github_issues[:closed].each do | _closed | 
+						if _closed["body"].include?(_issue[:md5])
+							debug_print "Found in #{ _closed[:comment] }, not posting\n"
+							cprint BOLD + " [" + RESET
+							cprint GREEN + BOLD + "Resolved on GitHub" + RESET
+							cprint BOLD + "]" + RESET
 						end
-						cprint "\n"
+						debug_print "Did not find in #{ _closed[:comment] }\n"
+					end	
 
+					debug_print "Checking if issue has been resolved\n"
+					@config.bitbucket_issues[:closed].each do  | _closed | 
+						if _closed["content"].include?(_issue[:md5])
+							debug_print "Found in #{ _closed["content"] }, not posting\n"
+							cprint BOLD + " [" + RESET
+							cprint GREEN + BOLD + "Resolved on Bitbucket" + RESET
+							cprint BOLD + "]\n" + RESET
+						end
+						debug_print "Did not find in #{ _closed["title"] }\n"
 					end
 					cprint "\n"
+
 				end
+				cprint "\n"
 			end
 		end
 	
