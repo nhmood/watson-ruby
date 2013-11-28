7283dcf72a84c0eb1f672f17a300b4eb46f366ad
alpaca-tc <alprhcp666@gmail.com>
2013-11-28 21:31:31 +0900

    Added json formatter

--- lib/watson/formatters.rb
+++ lib/watson/formatters.rb
@@ -1,4 +1,5 @@
 module Watson::Formatters
   autoload :BaseFormatter, 'watson/formatters/base_formatter'
   autoload :DefaultFormatter, 'watson/formatters/default_formatter'
+  autoload :JsonFormatter, 'watson/formatters/json_formatter'
 end
---
+++ lib/watson/formatters/json_formatter.rb
@@ -0,0 +1,17 @@
+require 'json'
+
+module Watson::Formatters
+  class JsonFormatter < BaseFormatter
+    def initialize(config)
+      @config = config
+    end
+
+    def run(structure)
+      debug_print "#{self} : #{__method__}\n"
+
+      File.open(@config.tmp_file, 'w') do |f|
+        f.write(structure.to_json)
+      end
+    end
+  end
+end
